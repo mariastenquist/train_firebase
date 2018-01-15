7b036e92e3c42f81b675bbcac6fec0e884ea1a4d
mariastenquist <stenquistm@gmail.com>
2018-01-14 23:52:55 -0700

    added train info

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,12 +11,47 @@
 
   var database = firebase.database();
 
+  database.ref().on("child_added", function(snapshot){
+    var firstTrainTime = snapshot.val().firstTime;
+    var frequency = snapshot.val().frequency;
+    var nextArrival = "";
+    var minutesAway;
+    var currentTime = moment();
+
+  var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+
+  car currentTime = moment();
+
+    console.log(firstTimeConverted);
+    console.log(firstTrainTime);
+    console.log(snapshot.val());
+    console.log(snapshot.val().name);
+    console.log(snapshot.val().frequency);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().firstTime);
+    // $("#tableBody").append('<tr><td>' + snapshot.val().name + '</td>' +
+    //   '<td>' + snapshot.val().destination + '<td>' +
+    //   '<td>' + snapshot.val().frequency + '<td>' +
+    //   '<td>' + snapshot.val().time + '</td>')
+    var newRow = `<tr>
+    <td>${snapshot.val().name}</td>
+    <td>${snapshot.val().destination}</td>
+    <td>${snapshot.val().frequency}</td>
+    <td>${nextTrain}</td>
+    <td></td>
+
+    </tr>`;
+    $("#tableBody").append(newRow);
+  }, function(errorObj) {
+    console.log("oopsie" + errorObj.code);  
+  });
+
 $("#submit").on("click", function(){
   event.preventDefault();
 
   var trainName = $("#trainNameId").val().trim();
   var trainDestination = $("#destinationId").val().trim();
-  var trainTime = $("#trainTimeId").val().trim();
+  var firstTrainTime = $("#trainTimeId").val().trim();
   var trainFrequency = parseInt($("#frequencyId").val().trim());
   // console.log(trainName);
   // console.log(trainDestination);
@@ -26,11 +61,13 @@ $("#submit").on("click", function(){
   var newTrain = {
     name: trainName,
     destination: trainDestination,
-    time: trainTime,
+    firstTime: firstTrainTime,
     frequency: trainFrequency
   };
   // console.log(newTrain);
   database.ref().push(newTrain);
-  $(".form-group" > input).val("");
+  $(".form-group > input").val("");
 });
+
+//look to the codersbay exercise to look at the value & snapshot example
 // console.log("hello");
